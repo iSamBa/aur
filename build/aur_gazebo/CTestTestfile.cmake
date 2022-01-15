54ff52c0a6ca1d3908d7a59a5b2e35fbcf898bb2
“Mohamad <mohamad.sayegh@pickit3d.com>
2022-01-15 18:21:58 +0100

    setting up and cleaning

---
+++ build/aur_gazebo/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamad/aur/src/aur_gazebo
+# Build directory: /home/mohamad/aur/build/aur_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/aur_navigation/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohamad/aur/src/aur_navigation
+# Build directory: /home/mohamad/aur/build/aur_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
